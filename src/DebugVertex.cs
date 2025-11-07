f175dbd9b74610bcd0a2b710ac6273e1bba7e1f8
Manuel <91590213+ManuelNoda@users.noreply.github.com>
2025-11-07 09:52:00 +0000

    Update DebugVertex.cs

--- src/DebugVertex.cs
+++ src/DebugVertex.cs
@@ -8,19 +8,21 @@ public class DebugVertex : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //TODO: EL GREEN ESTA CORRECTO
         cam = GetComponent<Camera>();
-        Vector4 pos = vertex[0].transform.position;
+        Vector4 pos = vertex[0].transform.localPosition;
         pos.w = 1;
-        Vector4 greenClipCoords = cam.projectionMatrix * cam.worldToCameraMatrix * vertex[0].transform.localToWorldMatrix * pos;
-        Debug.Log("Clip coords de vertex verde: " + greenClipCoords);
-        pos = vertex[1].transform.position;
+        Vector4 greenClipCoords = cam.projectionMatrix * cam.worldToCameraMatrix * vertex[0].transform.parent.localToWorldMatrix * pos;
+        Debug.Log("Clip coords de vertex verde: " + new Vector3((greenClipCoords.x / greenClipCoords.w + 1) / 2 * cam.pixelWidth, (greenClipCoords.y / greenClipCoords.w + 1) / 2 * cam.pixelHeight, greenClipCoords.z / greenClipCoords.w));
+        Debug.Log(cam.WorldToScreenPoint(vertex[0].transform.position ));
+        pos = vertex[1].transform.localPosition;
         pos.w = 1;
         Vector4 blueClipCoords = cam.projectionMatrix * cam.worldToCameraMatrix * vertex[1].transform.localToWorldMatrix * pos;
-        Debug.Log("Clip coords de vertex azul: " + blueClipCoords);
-        pos = vertex[2].transform.position;
+        Debug.Log("Clip coords de vertex azul: " + cam.WorldToScreenPoint(blueClipCoords));
+        pos = vertex[2].transform.localPosition;
         pos.w = 1;
         Vector4 redClipCoords = cam.projectionMatrix * cam.worldToCameraMatrix * vertex[2].transform.localToWorldMatrix * pos;
-        Debug.Log("Clip coords de vertex rojo: " + redClipCoords);
+        Debug.Log("Clip coords de vertex rojo: " + cam.WorldToScreenPoint(redClipCoords));
     }
 
     // Update is called once per frame
